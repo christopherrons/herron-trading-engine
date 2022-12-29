a75cfe60b2e3a14b45202633ee07e79e76ad585d
christopherrons <christopherheron09@gmail.com>
2022-12-29 10:50:13 +0100

    Initial commit

---
+++ src/test/java/com/herron/exchange/tradingengine/TradingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herron.exchange.tradingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
