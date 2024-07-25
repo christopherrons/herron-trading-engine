3cf0180885d42388d182524d7d5b95d9aeb40741
christopherrons <christopherheron09@gmail.com>
2024-07-25 15:27:39 +0200

    Update to java 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,18 +5,18 @@ include("trading-engine-deploy")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("spring.boot.starter.web", "org.springframework.boot:spring-boot-starter-web:3.0.1")
-            library("spring.boot.starter.parent", "org.springframework.boot:spring-boot-starter-parent:3.0.1")
-            library("spring.kafka", "org.springframework.kafka:spring-kafka:3.0.1")
+            library("spring.boot.starter.web", "org.springframework.boot:spring-boot-starter-web:3.2.0")
+            library("spring.boot.starter.parent", "org.springframework.boot:spring-boot-starter-parent:3.2.0")
+            library("spring.kafka", "org.springframework.kafka:spring-kafka:3.2.0")
             library("common.api", "com.herron.exchange:common-api:1.0.0-SNAPSHOT")
             library("common", "com.herron.exchange:common:1.0.0-SNAPSHOT")
         }
 
         create("testlibs") {
-            library("spring.boot.starter.test", "org.springframework.boot:spring-boot-starter-test:3.0.1")
+            library("spring.boot.starter.test", "org.springframework.boot:spring-boot-starter-test:3.2.0")
             library("junit.jupiter.api", "org.junit.jupiter:junit-jupiter-api:5.8.1")
             library("junit.jupiter.engine", "org.junit.jupiter:junit-jupiter-engine:5.8.1")
-            library("spring.kafka.test", "org.springframework.kafka:spring-kafka:3.0.1")
+            library("spring.kafka.test", "org.springframework.kafka:spring-kafka:3.2.0")
         }
     }
 }
